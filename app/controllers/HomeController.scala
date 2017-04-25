acba194713568bd5b8ec85a480c27eb74b78e0b9
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-04-25 17:08:11 -0400

    Added example of configuration: check use of configuration in HomeController, which uses property defined in conf/application.conf.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,25 @@
 package controllers
 
 import javax.inject._
+
 import play.api._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(configuration: Configuration) extends Controller {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    Ok(views.html.index(configuration.getString("example.greeting").getOrElse("Howdy!")))
   }
 }
