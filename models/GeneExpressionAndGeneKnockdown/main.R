ca457b9315c209feb5cd78ccc10a6e6b903d5e6c
vdancik <vdancik@broadinstitute.org>
2017-09-01 17:04:11 -0400

    update GeneExpressionAndGeneKnockdown model

--- models/GeneExpressionAndGeneKnockdown/main.R
+++ models/GeneExpressionAndGeneKnockdown/main.R
@@ -1,6 +1,12 @@
-library(bnlearn)
-library(readr)
+#!/usr/bin/env Rscript
+
+library(jsonlite)
+library(tidyjson)
 library(dplyr)
+library(readr)
+library(tidyr)
+library(rjson)
+library(rjags)
 
 ## read command line arguments
 args <- commandArgs(trailingOnly = T)
@@ -8,46 +14,95 @@ query_file <- args[1]
 output_file <- args[2]
 
 ## read model file
-pgm_file <- "bnlearn.discrete.cmap.kd.a375.96h.Rdata"
+pgm_file <- "jags.discrete.cmap.kd.a375.96h.Rdata"
 
-## parse query file and load model
-query <- read_tsv(query_file)
+# read JSON with jsonlite
+json_string <- read_file(query_file)
+query <- rjson::fromJSON(json_string)
+
+# parse input specification with tidyjson
+input <- json_string %>%
+  enter_object("modelInput") %>% 
+  gather_array %>% 
+  spread_values(variableGroupID = jstring("variableGroupID")) %>%
+  enter_object("modelVariable") %>%
+  gather_array %>%
+  spread_values(variableID = jstring("variableID")) %>%
+  enter_object("priorDistribution") %>%
+  enter_object("discreteDistribution") %>%
+  gather_array %>%
+  spread_values(variableValue = jstring("variableValue"), priorProbability = jstring("priorProbability")) %>%
+  select(-document.id, -array.index)
+
+# parse output specification
+output_spec <- json_string %>%
+  enter_object("modelOutput") %>%
+  gather_array %>%
+  spread_values(variableGroupID = jstring("variableGroupID"), rawOutput = jstring("rawOutput")) %>%
+  enter_object("variableID") %>%
+  gather_array %>%
+  append_values_string("variableID") %>%
+  mutate(variableID = gsub("gene knockdown", "gene_knockdown", variableID)) %>%
+  select(-document.id, -array.index)
+
+# separate input specification for two graph layers
+input_pert <- input %>% filter(variableGroupID == "GeneKnockdown" & priorProbability > 0)
+input_meas <- input %>% filter(variableGroupID == "GeneExpression" & priorProbability > 0) %>% mutate(variableValue = as.integer(factor(variableValue, levels = c("DN", "NC", "UP"))))
+
+
+### calculate posterior distribution
+## load model
 load(pgm_file)
 
-query <- query %>% mutate(variableName = gsub("gene knockdown", "gene_knockdown", variableName))
-input_pert <- query %>% filter(io == "input" & variableGroup == "GeneKnockdown" & probability > 0)
-input_meas <- query %>% filter(io == "input" & variableGroup == "GeneExpression" & probability > 0)
-output_template <- query %>% filter(io == "output") %>% mutate()
-
-## prepare evidence list
-evd <- list()
-if(nrow(input_pert) > 0){
-  evd[['gene_knockdown']] <- input_pert$variableValue
-}
-
+### run query (lw)
+## set evidence
+meas_obs <- list()
 if(nrow(input_meas) > 0){
   for(i in 1:nrow(input_meas)){
-    evd[[input_meas$variableName[i]]] <- input_meas$variableValue[i]
+    meas_obs[[input_meas$variableID[i]]] <- input_meas$variableValue[i]
   }
 }
 
-## run query
-query_dist <- cpdist(pgm, nodes = output_template$variableName, evidence = evd, method = "lw")
+## set data
+data <- c(list(meas = meas, p_gene_knockdown = p_gene_knockdown), meas_obs)
 
-## query
-output <- output_template[0,]
-counter <- 1
-for(i in 1:nrow(output_template)){
-  var_dist <- table(query_dist[, output_template$variableName[i]])/nrow(query_dist)
-  output <- rbind(output, output_template[rep(i, length(var_dist)),])
-  for(j in 1:length(var_dist)){
-    output$variableValue[counter] <- names(var_dist)[j]
-    output$probability[counter] <- var_dist[j]
-    counter <- counter + 1
+# set model
+model <- jags.model(textConnection(modelstring), data=data)
+
+# run model
+update(model,n.iter=10)
+
+# get results
+query_dist <- coda.samples(model=model,variable.names=output_spec$variableID, n.iter=100, thin=1)
+
+### process query results
+## create list form results that can be written to JSON
+uGroup <- unique(output_spec$variableGroupID)
+output <- list(posteriorProbability = vector(mode = "list", length = length(uGroup)))
+for(i in 1:length(uGroup)){
+  os_sub <- output_spec %>% filter(variableGroupID == uGroup[i])
+  
+  output$posteriorProbability[[i]]$variableGroupID <- uGroup[i]
+  output$posteriorProbability[[i]]$modelVariable <- vector(mode = "list", length = nrow(os_sub))
+  
+  for(j in 1:nrow(os_sub)){
+    var_dist <- table(query_dist[[1]][, os_sub$variableID[j]])/nrow(query_dist[[1]])
+    
+    if(os_sub$variableGroupID == "GeneExpression"){
+      names(var_dist) <- c("DN", "NC", "UP")[as.numeric(names(var_dist))]
+    } else if (os_sub$variableGroupID == "GeneKnockdown") {
+      names(var_dist) <- dimnames(meas)[[3]][as.numeric(names(var_dist))]
+    }
+    
+    output$posteriorProbability[[i]]$modelVariable[[j]]$variableID = os_sub$variableID[j]
+    output$posteriorProbability[[i]]$modelVariable[[j]]$posteriorDistribution <- list("discreteDistribution" = vector(mode = "list", length = length(var_dist)))
+    
+    for(k in 1:length(var_dist)){
+      output$posteriorProbability[[i]]$modelVariable[[j]]$posteriorDistribution$discreteDistribution[[k]]$variableValue <- names(var_dist)[k]
+      output$posteriorProbability[[i]]$modelVariable[[j]]$posteriorDistribution$discreteDistribution[[k]]$posteriorProbability <- var_dist[[k]]
+    }
   }
 }
 
-output <- output %>% mutate(variableName = gsub("gene_knockdown", "gene knockdown", variableName))
-
-## write results
-write.table(output, output_file, row.names = F, quote=F, sep = "\t")
+## write JSON
+write(prettify(rjson::toJSON(output)), file = output_file)
